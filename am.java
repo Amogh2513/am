cf505090821357c6c3712f96737abf4b07e753d6
Amogh2513 <165883094+Amogh2513@users.noreply.github.com>
2024-05-11 15:07:22 +0530

    Update am.java

--- am.java
+++ am.java
@@ -1,8 +1,8 @@
 public class am {
 
 	public static void main(String[] args) {
-        system.out.println("hello world")
-        system.out.println("hello")
-        system.out.println("hello")
+        system.out.println("hello world");
+        system.out.println("hello");
+        system.out.println("hello");
     }
 }
